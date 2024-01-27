3148010fcb9b7f315031cfda548ce1a7e947e935
e-davies <emmi.lou002@gmail.com>
2024-01-27 15:28:06 +0000

    adding comments for TODOs

--- portfolio/src/components/Footer.jsx
+++ portfolio/src/components/Footer.jsx
@@ -1,9 +1,20 @@
+/*
+Need to understand how to import and use fontawesome icons to replace <i>'s below
+*/
 
+
+import React from "react";
 
 function Footer() {
     return (
-        <div></div>
+        <footer>
+            <div>
+                <a href="https://www.linkedin.com/" target="_blank"><i className="fa-brands fa-linkedin-in" alt="linkedin logo"></i></a>
+                <a href="https://github.com/E-Davies/" target="_blank"><i className="fa-brands fa-github" alt="github logo"></i></a>
+                <a href="https://www.instagram.com/" target="_blank"><i className="fa-brands fa-instagram" alt="instagram logo"></i></a>
+            </div> 
+        </footer>
     );
 };
-  
-  export default Footer;
+
+export default Footer;
--- portfolio/src/components/pages/Home.jsx
+++ portfolio/src/components/pages/Home.jsx
@@ -7,6 +7,23 @@ Home:
         - Some indication that this is your portfolio site
 */
 
+/*
+how to add images? 
+    - bg img to jumbotron
+    - profile img
+    - work project imgs
+*/
+
+/*
+Should I do components for: 
+    - Skills
+    - work projects? (guess these should be in projects page and load components ar projectGallery)
+    
+*/
+
+
+
+
 function Home() {
     return (
         <div>
@@ -27,21 +44,65 @@ function Home() {
             </div>
 
             <section id="skills">
-            <h2>Skills</h2>
-
-            <div class="container-fluid text-center">
-                <div class="row g-3">
-                    <div class="col-lg-5 col-sm-12 p-3 m-md-auto my-md-4 skills-box">HTML</div>
-                    <div class="col-lg-5 col-sm-12 p-3 m-md-auto my-md-4 skills-box">CSS</div>
-                    <div class="col-lg-5 col-sm-12 p-3 m-md-auto my-md-4 skills-box">Bootstrap</div>
-                    <div class="col-lg-5 col-sm-12 p-3 m-md-auto my-md-4 skills-box">JavaScript</div>
-                    <div class="col-lg-5 col-sm-12 p-3 m-md-auto my-md-4 skills-box">Node.js</div>
-                    <div class="col-lg-5 col-sm-12 p-3 m-md-auto my-md-4 skills-box">React</div>
-                    <div class="col-lg-5 col-sm-12 p-3 m-md-auto my-md-4 skills-box">Git</div>
-                    <div class="col-lg-5 col-sm-12 p-3 m-md-auto my-md-4 skills-box">GitHub</div>
+                <h2>Skills</h2>
+{/* Could these be components?? */}
+                <div class="container-fluid text-center">
+                    <div class="row g-3">
+                        <div class="col-lg-5 col-sm-12 p-3 m-md-auto my-md-4 skills-box">HTML</div>
+                        <div class="col-lg-5 col-sm-12 p-3 m-md-auto my-md-4 skills-box">CSS</div>
+                        <div class="col-lg-5 col-sm-12 p-3 m-md-auto my-md-4 skills-box">Bootstrap</div>
+                        <div class="col-lg-5 col-sm-12 p-3 m-md-auto my-md-4 skills-box">JavaScript</div>
+                        <div class="col-lg-5 col-sm-12 p-3 m-md-auto my-md-4 skills-box">Node.js</div>
+                        <div class="col-lg-5 col-sm-12 p-3 m-md-auto my-md-4 skills-box">React</div>
+                        <div class="col-lg-5 col-sm-12 p-3 m-md-auto my-md-4 skills-box">Git</div>
+                        <div class="col-lg-5 col-sm-12 p-3 m-md-auto my-md-4 skills-box">GitHub</div>
+                    </div>
                 </div>
-            </div>
+            </section>
 
+            <section id="work">
+                <h2>Work</h2>
+
+                <div class="container-fluid">
+                    <div class="row ms-3 me-3">
+                        
+                        {/* <!-- card 1 --> */}
+                        <div class="cardContainer col-lg-4 col-sm-12 my-lg-5 my-sm-4">
+                            <div class="card">
+                                <img src="./images/snake2.jpg" class="card-img-top" alt="snake game board" />
+                                <div class="card-body">
+                                    <h5 class="card-title">Snake Game</h5>
+                                    <p class="card-text">The classic Snake game. This game was created using the YouTube tutorial by Web Dev Simplified - 'How To Code The Snake Game In Javascript'.</p>
+                                    <a href="https://e-davies.github.io/snake/" target="_blank" class="btn btn-primary">Play Snake</a>
+                                </div>
+                            </div>
+                        </div>
+
+                        {/* <!-- card 2 --> */}
+                        <div class="cardContainer col-lg-4 col-sm-12 my-lg-5 my-sm-4">
+                            <div class="card">
+                                <img src="./images/drum-kit.JPG" class="card-img-top" alt="keyboard drum kit" />
+                                <div class="card-body">
+                                    <h5 class="card-title">Drum Kit</h5>
+                                    <p class="card-text">A drum kit you can use with your keyboard! See how much noise you can make 🥁. This was a project from the Wes Bos' JavaScript 30 course - a 30 day vanilla JS coding challenge.</p>
+                                    <a href="https://e-davies.github.io/drum-kit/" target="_blank" class="btn btn-primary">Come make some noise!</a>
+                                </div>
+                            </div>
+                        </div>
+
+                        {/* <!-- card 3 --> */}
+                        <div class="cardContainer col-lg-4 col-sm-12 my-lg-5 my-sm-4">
+                            <div class="card">
+                                <img src="./images/pig-dice-game.JPG" class="card-img-top" alt="2 player dice game board" />
+                                <div class="card-body">
+                                <h5 class="card-title">Pig Dice Game</h5>
+                                <p class="card-text">A simple 2 player dice game where the first player to score 100 or more points wins. But beware, you don't want to roll a 1!</p>
+                                <a href="https://e-davies.github.io/pig-dice-game/" target="_blank" class="btn btn-primary">Roll the dice</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </section>
         </div>
     );
