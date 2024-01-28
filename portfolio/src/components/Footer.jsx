7bd2fc4a3919ff8e3719f25dbcf08c46a6f0c29e
e-davies <emmi.lou002@gmail.com>
2024-01-28 09:30:28 +0000

    fixed bug with fontawesome icons not showing

--- portfolio/src/components/Footer.jsx
+++ portfolio/src/components/Footer.jsx
@@ -4,14 +4,20 @@ Need to understand how to import and use fontawesome icons to replace <i>'s belo
 
 
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faLinkedinIn, faGithub, faInstagram } from '@fortawesome/free-solid-svg-icons'
+import { faLinkedinIn, faGithub, faInstagram } from '@fortawesome/free-brands-svg-icons'
 
 function Footer() {
     return (
         <footer>
             <div>
-                <a href="https://www.linkedin.com/" target="_blank"><i className="fa-brands fa-linkedin-in" alt="linkedin logo"></i></a>
+                {/* <a href="https://www.linkedin.com/" target="_blank"><i className="fa-brands fa-linkedin-in" alt="linkedin logo"></i></a>
                 <a href="https://github.com/E-Davies/" target="_blank"><i className="fa-brands fa-github" alt="github logo"></i></a>
-                <a href="https://www.instagram.com/" target="_blank"><i className="fa-brands fa-instagram" alt="instagram logo"></i></a>
+                <a href="https://www.instagram.com/" target="_blank"><i className="fa-brands fa-instagram" alt="instagram logo"></i></a> */}
+                <a href="https://www.linkedin.com/" target="_blank"><FontAwesomeIcon icon={faLinkedinIn} /></a>
+                <a href="https://github.com/E-Davies/" target="_blank"><FontAwesomeIcon icon={faGithub} /></a>
+                <a href="https://www.instagram.com/" target="_blank"><FontAwesomeIcon icon={faInstagram} /></a>
             </div> 
         </footer>
     );
--- portfolio/src/components/NavTabs.jsx
+++ portfolio/src/components/NavTabs.jsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 
 function NavTabs() {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className="navbar navbar-expand-md">
             <NavLink 
                 to="/" 
                 // end 
