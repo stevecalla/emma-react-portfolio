57cc052e78b97161b3da2806ba7ac91adadcb4d2
Steve Calla <callasteven@gmail.com>
2024-03-04 14:44:06 -0700

    Props validation eslint

--- portfolio/src/components/Project.jsx
+++ portfolio/src/components/Project.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable react/jsx-no-target-blank */
 // function Project(props) {
 function Project({ img, descr, title, deployedLink, githubLink, btn }) {
