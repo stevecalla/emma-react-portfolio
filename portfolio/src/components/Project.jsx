1dc2c495f653619092860e1dca538b867bdeafb9
Steve Calla <callasteven@gmail.com>
2024-03-04 14:39:52 -0700

    Adjust images

--- portfolio/src/components/Project.jsx
+++ portfolio/src/components/Project.jsx
@@ -1,22 +1,36 @@
-function Project(props) {
-  console.log(props);
-    return (
-        <div className="cardContainer col-lg-4 col-sm-12 my-lg-5 my-sm-4">
-            <div className="card">
-                <img src={'./images/'+props.img} className="card-img-top" alt="snake game board" />
-                <div className="card-body d-flex flex-column justify-content-between">
-                    <div>
-                        <h5 className="card-title">{props.title}</h5>
-                        <p className="card-text">{props.descr}</p>
-                    </div>
-                    <div className="mt-3">
-                        <a href={props.deployedLink} target="_blank" className="btn btn-primary d-block">{props.btn}</a>
-                        <a href={props.githubLink} target="_blank" className="btn btn-primary mt-2 d-block">Go to GitHub Repo</a>
-                    </div>
-                </div>
-            </div>
+/* eslint-disable react/jsx-no-target-blank */
+// function Project(props) {
+function Project({ img, descr, title, deployedLink, githubLink, btn }) {
+  console.log(img);
+  return (
+    <div className="cardContainer col-lg-4 col-sm-12 my-lg-5 my-sm-4">
+      <div className="card">
+        <img src={img} className="card-img-top" alt="snake game board" />
+        <div className="card-body d-flex flex-column justify-content-between">
+          <div>
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{descr}</p>
+          </div>
+          <div className="mt-3">
+            <a
+              href={deployedLink}
+              target="_blank"
+              className="btn btn-primary d-block"
+            >
+              {btn}
+            </a>
+            <a
+              href={githubLink}
+              target="_blank"
+              className="btn btn-primary mt-2 d-block"
+            >
+              Go to GitHub Repo
+            </a>
+          </div>
         </div>
-    );
-};
-  
+      </div>
+    </div>
+  );
+}
+
 export default Project;
